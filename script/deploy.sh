499f03c222313f4a7f1e7172d507654f36590f79
Slathia <slathiasachin13@gmail.com>
2018-08-29 18:18:02 +0530

    master origin

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,6 @@
 rm -r assignment-1.0.SNAPSHOT | echo "artifact already deleted"
 
-unzip dummy/assignment-1.0-SNAPSHOT.zip -d 
+unzip assignment-1.0-SNAPSHOT.zip -d 
   
 ./assignment-1.0-SNAPSHOT/bin/assignment &
 

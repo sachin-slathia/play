e21399539d31281caf0dc5338d0603fc8d7e92e6
Slathia <slathiasachin13@gmail.com>
2018-08-30 14:44:52 +0530

    Script file changed

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,7 @@
+
+
+ps -ef | grep -i assignment-1.0-SNAPSHOT | grep -v grep | awk '{print $2}' | xargs kill
+
 rm -r assignment-1.0.SNAPSHOT | echo "artifact already deleted"
 
 unzip assignment-1.0-SNAPSHOT.zip 

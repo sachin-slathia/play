ecb9076065964efb390be8ed19849e3ca0264835
Slathia <slathiasachin13@gmail.com>
2018-08-30 14:24:38 +0530

    Script file changed

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,7 @@ rm -r assignment-1.0.SNAPSHOT | echo "artifact already deleted"
 
 unzip assignment-1.0-SNAPSHOT.zip 
 
+rm -r assignment-1.0-SNAPSHOT.zip 
+
 ./assignment-1.0-SNAPSHOT/bin/assignment &
 

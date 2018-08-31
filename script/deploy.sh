a6cba0948fe534340fb960c9d11f2ff56983a9ca
Slathia <slathiasachin13@gmail.com>
2018-08-31 17:26:32 +0530

    changes in script file

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,4 @@
 
+sudo run -d -p 9000:9000 play 
 
-ps -ef | grep -i play | grep -v grep | awk '{print $2}' | xargs kill
-
-rm -r assignment-1.0.SNAPSHOT | echo "artifact already deleted"
-
-unzip assignment-1.0-SNAPSHOT.zip 
-
-rm -r assignment-1.0-SNAPSHOT.zip 
-
-./assignment-1.0-SNAPSHOT/bin/assignment &
 
